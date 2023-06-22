e03641cee019de4d886bc2b49c798c0b46de6139
Joshua Young <joshua.young@kainos.com>
2023-06-22 11:25:45 +0100

    josh section

--- init_script.sql
+++ init_script.sql
@@ -31,6 +31,40 @@ BEGIN
 		FOREIGN KEY (lead_employee_id) REFERENCES employees(id)
 	);
 
+		
+	CREATE TABLE IF NOT EXISTS projects (
+	id SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	client_id SMALLINT UNSIGNED NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	value INT UNSIGNED NOT NULL,
+	technologies TEXT,
+	started_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	completed_at TIMESTAMP,
+
+	FOREIGN KEY (client_id) REFERENCES client(id)
+	);        
+			
+	CREATE TABLE IF NOT EXISTS project_employees (
+	project_id SMALLINT UNSIGNED NOT NULL,
+	client_id SMALLINT UNSIGNED NOT NULL,
+	joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+	CONSTRAINT pk_project_client PRIMARY KEY (project_id, client_id),
+	FOREIGN KEY (project_id) REFERENCES projects(id),
+	FOREIGN KEY (client_id) REFERENCES client(id)
+	);     
+			
+	CREATE TABLE IF NOT EXISTS project_employee_logs (
+	project_id SMALLINT UNSIGNED NOT NULL,
+	client_id SMALLINT UNSIGNED NOT NULL,
+	joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	left_at TIMESTAMP,
+
+	CONSTRAINT pk_project_client PRIMARY KEY (project_id, client_id),
+	FOREIGN KEY (project_id) REFERENCES project(id),
+	FOREIGN KEY (client_id) REFERENCES client(id)
+	);       
+
 	-- check the number of affected rows
 	GET DIAGNOSTICS @rows = ROW_COUNT;
 	IF @rows = 0 THEN
