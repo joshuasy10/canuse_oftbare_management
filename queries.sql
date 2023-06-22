6c072fc1a5e90a13d92ff6bfe4562913f6069ee9
AlexMcC02 <alex.mccullough@kainos.com>
2023-06-22 14:04:22 +0100

    queries

---
+++ queries.sql
@@ -0,0 +1,22 @@
+-- Task 9: Get a list of clients, name of the sales employee who with that client and the list of projects that the client has.
+
+-- Task 10: Get the client who has the highest value of projects.
+
+-- Task 11: Get the client who has the lowest value of projects.
+
+-- Task 12: Get a list of clients with the average value of their projects.
+
+-- Task 13: Get a list of sales employees and how much each has made in commission this year.
+
+-- Task 14: Get a list of sales employees who haven't won any clients this year.
+
+-- Task 15: Get a list of delivery employees who aren't assigned to a project, 
+-- who have previously worked with a technology that is currently being used on 
+-- a project and the name of those projects.
+
+-- Task 16: Set a project as completed (completed projects shouldn't show in any lists)
+
+-- Task 17: See a list of projects, the name of the Tech Lead and a list of all 
+-- delivery employees assigned to the project.
+
+-- Task 18: See the project with the most delivery employees working on it.
