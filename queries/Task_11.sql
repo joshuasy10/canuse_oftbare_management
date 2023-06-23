8af8f8d265703a96d407c739083c4cb5fb7a9a16
AlexMcC02 <alex.mccullough@kainos.com>
2023-06-23 09:00:59 +0100

    more progress on queries

---
+++ queries/Task_11.sql
@@ -0,0 +1,6 @@
+SELECT clients.id, clients.name, SUM(projects.value) AS 'Total Value'
+FROM clients
+JOIN projects ON clients.id = projects.client_id
+GROUP BY clients.id, clients.name
+ORDER BY `Total Value` ASC
+LIMIT 1;
